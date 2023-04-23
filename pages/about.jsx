501af97bc47110ecee06306dde3a4bcb77e28b00
mohamed7911 <81492158+mohamed7911@users.noreply.github.com>
2023-04-23 23:23:37 +0200

    Update about.jsx

--- pages/about.jsx
+++ pages/about.jsx
@@ -2,10 +2,10 @@ const AboutPage = () => {
   return (
     <>
       <h3>A Little Bit About Me</h3>
-      <p style={{lineHeight:3}}>I am passionate about Front-end development, Back-end development.</p>
+      <p style={{lineHeight:3}}>I am passionate about Front-end development & Back-end development.</p>
       <p style={{lineHeight:3}}>
-        I have a wide range of knowledge and skills. My goal is to continue to
-        learn and master new technologies while utilizing my existing skills in
+        I have a wide range of knowledge and skills. My goal is to continue
+        learning and master new technologies while utilizing my existing skills in
         a positive team-oriented work environment that rewards hard work and
         commitment.
       </p>
